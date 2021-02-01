087200d6883f8444e4739c1d6e7e1ec2f7b5dc97
areebaqamar021 <areebaqamar021@gmail.com>
2021-02-01 11:47:29 +0500

    gatsby demo project

---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Student-PC\\OneDrive\\Desktop\\areeba-projects\\demo-project\\.cache\\dev-404-page.js"))),
+  "component---src-templates-advanced-js": hot(preferDefault(require("C:\\Users\\Student-PC\\OneDrive\\Desktop\\areeba-projects\\demo-project\\src\\templates\\advanced.js"))),
+  "component---src-templates-blog-js": hot(preferDefault(require("C:\\Users\\Student-PC\\OneDrive\\Desktop\\areeba-projects\\demo-project\\src\\templates\\blog.js"))),
+  "component---src-templates-page-js": hot(preferDefault(require("C:\\Users\\Student-PC\\OneDrive\\Desktop\\areeba-projects\\demo-project\\src\\templates\\page.js"))),
+  "component---src-templates-portfolio-js": hot(preferDefault(require("C:\\Users\\Student-PC\\OneDrive\\Desktop\\areeba-projects\\demo-project\\src\\templates\\portfolio.js"))),
+  "component---src-templates-post-js": hot(preferDefault(require("C:\\Users\\Student-PC\\OneDrive\\Desktop\\areeba-projects\\demo-project\\src\\templates\\post.js"))),
+  "component---src-templates-project-js": hot(preferDefault(require("C:\\Users\\Student-PC\\OneDrive\\Desktop\\areeba-projects\\demo-project\\src\\templates\\project.js")))
+}
+
